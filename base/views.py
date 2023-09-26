7d4fe3c6efe1bc9b6617cb0b8febf2d6bc79514e
Felipe Oliveira <flipeaz342@gmail.com>
2023-09-26 17:27:43 -0400

    add função login

--- base/views.py
+++ base/views.py
@@ -1,13 +1,47 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.contrib import messages
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.T
 
 
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist.")
+
+        user =authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist.')
+
+    return render(request, 'base/login_register.html')
+
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__contains=q) |
+        Q(description__contains=q)
+        ) 
+
+    room_count = rooms.count() 
+
+    topics = Topic.objects.all()
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 
@@ -48,4 +82,5 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
 
-    return render(request, 'base/delete.html', {'obj': room})
+    context = {'obj': room}
+    return render(request, 'base/delete.html', context)
