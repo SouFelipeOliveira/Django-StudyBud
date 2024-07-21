4543ff40841473d12eb66c0bfc8bb72c47381975
Felipe Oliveira <flipeaz342@gmail.com>
2024-07-21 15:56:52 -0400

    change UserForm

--- base/forms.py
+++ base/forms.py
@@ -2,13 +2,25 @@ from django.forms import ModelForm
 from .models import Room
 from django.contrib.auth.models import User
 
+
 class RoomForm(ModelForm):
     class Meta:
         model = Room
-        fields = '__all__'
-        exclude = ['host', 'participants']
+        fields = "__all__"
+        exclude = ["host", "participants"]
+
 
 class UserForm(ModelForm):
     class Meta:
-        model= User
-        fields = ["username", "email"]
+        model = User
+        fields = "__all__"
+        exclude = [
+            "groups",
+            "last_login",
+            "password",
+            "is_staff",
+            "is_active",
+            "date_joined",
+            "user_permissions",
+            "is_superuser"
+        ]
