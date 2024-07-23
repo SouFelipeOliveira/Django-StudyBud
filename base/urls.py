3a60d0896191ce16b46ba61427d7e0b231b8be5a
Felipe Oliveira <flipeaz342@gmail.com>
2024-07-23 00:24:03 -0400

    password reset by email

--- base/urls.py
+++ base/urls.py
@@ -1,26 +1,48 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
-    path('login/', views.loginPage, name="login"),
-    path('logout/', views.logoutUser, name="logout"),
-    path('register/', views.registerUser, name="register"),
+    path("login/", views.loginPage, name="login"),
+    path("logout/", views.logoutUser, name="logout"),
+    path("register/", views.registerUser, name="register"),
+    path("", views.home, name="home"),
+    path("room/<str:pk>/", views.room, name="room"),
+    path("profile/<str:pk>/", views.userProfile, name="user-profile"),
+    path("create-room/", views.createRoom, name="create-room"),
+    path("update-room/<str:pk>/", views.updateRoom, name="update-room"),
+    path("delete-room/<str:pk>/", views.deleteRoom, name="delete-room"),
+    path("delete-message/<str:pk>/", views.deleteMessage, name="delete-message"),
+    path("update-user/<str:pk>/", views.updateUser, name="update-user"),
+    path("topics/", views.topicsPage, name="topics"),
+    path("activity/", views.activityPage, name="activity"),
+]
 
-    path('', views.home, name="home"),
-    path('room/<str:pk>/', views.room, name="room"),
-    path('profile/<str:pk>/', views.userProfile, name="user-profile"),
-
-    path('create-room/', views.createRoom, name="create-room"),
-    path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
-    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
-    path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
-    path('update-user/<str:pk>/', views.updateUser, name="update-user"),
-    path('topics/', views.topicsPage, name="topics"),
-    path('activity/', views.activityPage, name="activity")
+reset_password = [
+    path(
+        "password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
 
 erros = [
     path("403/", views.permissionDeniedView, name="permission-denied"),
 ]
 
-urlpatterns + erros
+
+urlpatterns += reset_password + erros
